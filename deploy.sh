73eb34411b732b4d2669f5b562e6e8cc3f5ea690
gavinzhou <gavin.zhou@gmail.com>
2016-08-29 14:26:14 +0000

    backroll mistake

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,6 @@ for path in $dockerfiles; do
   # Generate a tag by replacing the first slash with a colon and all remaining slashes with a dash.
   tag=$(echo $path | sed 's@/@:@' | sed 's@/@-@g')
   log_msg "Tagging docker image $tag with gcr tag"
-  docker push "orangesys/alpine-postgres:${tag}"
   docker tag "orangesys/alpine-postgres:${tag}" "asia.gcr.io/orange-sys/alpine-postgres:${tag}"
   sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/orange-sys/alpine-postgres:${tag}
 done
